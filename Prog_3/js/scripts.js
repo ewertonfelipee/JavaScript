210880245a8339279d1777730de444d2577e9657
ewertonfelipee <ewertonfmarinho@protonmail.ch>
2022-05-11 18:26:08 -0300

    Prog 3

---
+++ Prog_3/js/scripts.js
@@ -0,0 +1,10 @@
+console.log("First to be printed");
+console.log("Second to be printed");
+
+var a = 8;
+var b = 16;
+var c = a + b;
+console.log("Result will be: " + c);
+
+a = "teste";
+console.log(a);
