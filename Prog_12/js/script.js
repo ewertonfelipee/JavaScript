efb63fb7ca341aa6f129e2fda44226e1654875b2
ewertonfelipee <ewertonfmarinho@protonmail.ch>
2022-05-22 20:02:49 -0300

    sem mensagem

---
+++ Prog_12/js/script.js
@@ -0,0 +1,11 @@
+var numero = 20;
+
+if(numero === 19){
+    console.log("Eh um igual a 19 e o tipo eh number");
+}
+else if(numero === "19"){
+    console.log("Nao eh do tipo number");
+}
+else if(numero !== 19){
+    console.log("Nao eh 19 do tipo number");
+}
