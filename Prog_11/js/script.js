72da36f64d37b506b1ea5b33e41b30482af4ed25
ewertonfelipee <ewertonfmarinho@protonmail.ch>
2022-05-22 18:53:18 -0300

    Condicionais

---
+++ Prog_11/js/script.js
@@ -0,0 +1,11 @@
+var idade = 18;
+var idadeMinima = 18;
+if(idade >= idadeMinima){
+    console.log("Pode entrar na festa");
+}
+else if(idade >= 16 && idade <= 17){
+    console.log("Precisa de altorizacao dos pais");
+}
+else{
+    console.log("Nao pode entrar na festa");
+}
