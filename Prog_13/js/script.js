809c5b1fec5f19685c40d81d69cd6ebe28a89176
ewertonfelipee <ewertonfmarinho@protonmail.ch>
2022-05-26 19:29:53 -0300

    Estrutura while

---
+++ Prog_13/js/script.js
@@ -0,0 +1,13 @@
+var i = 0;
+while (i <= 10){
+    print("conte ate " + i);
+    i++;
+}
+print("\n");
+
+var arr = ["Ewerton", "Felipe", 23, "Graduando"];
+var x = 0;
+while(x < arr.length){
+    print(arr[x]);
+    x++;
+}
