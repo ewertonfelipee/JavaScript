0e1ff88950d043ee7f15d96a3d132bca50580daa
ewertonfelipee <ewertonfmarinho@protonmail.ch>
2022-05-26 21:04:32 -0300

    My first function

---
+++ Prog_15/function.js
@@ -0,0 +1,14 @@
+// Functions
+var nome = "Ewerton";
+function firstFunction(nome){
+    print("My name is " + nome);
+}
+firstFunction(nome);
+
+function sum(a, b){
+    let sum = a + b;
+    return sum;
+}
+print(sum(12, 32));
+const soma = sum(15,65);
+print(soma);
