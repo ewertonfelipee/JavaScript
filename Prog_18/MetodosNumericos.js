c8e9cf1279c62811fc2f7d9299d9894893afcd81
ewertonfelipee <ewertonfmarinho@protonmail.ch>
2022-06-01 22:28:44 -0300

    Number Methods

---
+++ Prog_18/MetodosNumericos.js
@@ -0,0 +1,17 @@
+//  parseFloat
+
+print(parseFloat("10.33"));
+
+//  parseInt
+
+print(parseInt("10.432"));
+print(parseInt(10.432));
+
+//  toFixed
+
+print(23.05.toFixed(1));
+
+//  MAX_VALUE and MIN_VALUE
+
+print(Number.MAX_VALUE);
+print(Number.MIN_VALUE);
