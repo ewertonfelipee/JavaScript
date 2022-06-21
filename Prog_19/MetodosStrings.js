62aabe95ee28582a103cb62a95af38b42f27d165
ewertonfelipee <ewertonfmarinho@protonmail.ch>
2022-06-21 20:58:11 -0300

    add search() and includes methods at the file

--- Prog_19/MetodosStrings.js
+++ Prog_19/MetodosStrings.js
@@ -1,23 +1,34 @@
-const firstName = "Ewerton";
-print(firstName.length);
+// const firstName = "Ewerton";
+// print(firstName.length);
 
-print(firstName.substring(0,4));
+// print(firstName.substring(0,4));
 
-print(firstName.toUpperCase());
+// print(firstName.toUpperCase());
 
-print(firstName.toLowerCase());
+// print(firstName.toLowerCase());
 
-const fullName = "Ewerton Felipe";
+// const fullName = "Ewerton Felipe";
 
-print(fullName.slice(0,9));
+// print(fullName.slice(0,9));
 
-const lastName = "Silva";
+// const lastName = "Silva";
 
-const nameFull = fullName.replace("Felipe", lastName);
-print(nameFull);
+// const nameFull = fullName.replace("Felipe", lastName);
+// print(nameFull);
 
-const text = "      hello       ";
-print("Before trim():" + text);
-print("After trim(): " + text.trim());
+// const text = "      hello       ";
+// print("Before trim():" + text);
+// print("After trim(): " + text.trim());
 
-print(fullName.split("  "));
+// print(fullName.split("  "));
+
+//  Javascript string search() method
+let srt = "ewerton felipe silva marinho";
+print(srt.search("silva"));
+
+//  The includes() method returns true if a string contains a specified value.
+let string = "ola mundo";
+print(string.includes("ola"));
+print(string.includes("vermelho"));
+print(string.includes("ola", 0));   // check the string includes "ola" starting position 0
+print(string.includes("ola", 2));
