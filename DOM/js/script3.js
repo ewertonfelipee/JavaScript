f56a82aab1f1bf481dea9c4dbf32e45a1f58cb87
ewertonfelipee <ewertonfmarinho@protonmail.ch>
2022-07-19 16:18:33 -0300

    Changing CSS styles

---
+++ DOM/js/script3.js
@@ -0,0 +1,4 @@
+//  Changing HTML Style
+document.getElementById("p1").style.color = "blue";
+document.getElementById("p1").style.fontFamily = "Arial";
+document.getElementById("p1").style.fontSize = "65px";
