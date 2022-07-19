e73a53018ec45a803d1562b3ba51c5b140daad44
ewertonfelipee <ewertonfmarinho@protonmail.ch>
2022-07-19 18:35:56 -0300

    onchange, onmouseouver and onmouseout

---
+++ JS-Events/js/script2.js
@@ -0,0 +1,12 @@
+upperCase = () => {
+  const up = document.getElementById("fname");
+  up.value = up.value.toUpperCase();
+}
+
+mOver = obj => {
+  obj.innerHTML = "mouse over";
+}
+
+mOut = obj2 => {
+  obj2.innerHTML = "mouse out";
+}
