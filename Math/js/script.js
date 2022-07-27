f83973d65805022dec3393335547b3580a05f8b4
ewertonfelipee <ewertonfmarinho@protonmail.ch>
2022-07-27 18:31:49 -0300

    Math methods

---
+++ Math/js/script.js
@@ -0,0 +1,23 @@
+//  properties(constants)
+console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.SQRT2);
+
+//  Mathods
+console.log(Math.round(5.63));  //  returns the nearest integer
+console.log(Math.ceil(5.63));   //  returns the value of x rounded up to its nearest integer
+console.log(Math.floor(5.63));  //  returns the value of x rounded down to its nearest integer
+console.log(Math.trunc(5.63));  //  returns the integer part of x
+console.log(Math.sign(-19));    //  returns -1
+console.log(Math.sign(0));      //  returns 0
+console.log(Math.sign(19));     //  returns 1
+console.log(Math.pow(5,5));
+console.log(Math.sqrt(4));
+console.log(Math.abs(-90));     //  returns absolute value of x
+console.log(Math.abs(90));
+console.log(Math.min(-100, 100, -43, -54, 93, 3));
+console.log(Math.max(-100, 100, -43, -54, 93, 3));
+console.log(Math.random());
+
+//  JS Random()
+console.log(Math.floor(Math.random() * 11));
